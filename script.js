a905e73efe94032344f9116f962750c1370b7d93
Bewizi <toluwalase.bewaji@yahoo.com>
2024-05-22 19:29:12 +0100

    added property header (h2) and (p) tag

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const servieSection = document.querySelector("#service");
 // Sticky Header
 const stickyHeader = function (entries) {
   const [entry] = entries;
-  console.log(entry);
   if (!entry.isIntersecting) nav.classList.add("sticky");
   else nav.classList.remove("sticky");
 };
