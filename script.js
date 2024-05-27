b68b04e7d99bf97f01a36626df5d986b26147d46
Bewizi <toluwalase.bewaji@yahoo.com>
2024-05-27 16:01:43 +0100

    partner section worked on made the prev and next button functional

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const allServices = document.querySelectorAll(".services");
 const clientOne = document.querySelector(".client__1");
 const clientTwo = document.querySelector(".client__2");
 const clientThree = document.querySelector(".client__3");
+const clientsMessages = document.querySelectorAll(".client__message");
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
 
 // Sticky Header
 const stickyHeader = function (entries) {
@@ -34,12 +37,7 @@ Services.addEventListener("click", function (e) {
   serviceClicked.classList.add("active");
 });
 
-// Testimonial
-const clientsMessages = document.querySelectorAll(".client__message");
-const arrow = document.querySelector(".arrow");
-const leftArrow = document.querySelector(".left-arrow");
-const rightArrow = document.querySelector(".right-arrow");
-
+// Testimonial Section
 let currentIndex = 1;
 
 function updateClientDisplay(index) {
@@ -96,3 +94,81 @@ clientsMessages.forEach((clientsMessage, index) => {
 //   clientTwo.classList.add("active");
 //   clientThree.classList.add("blur");
 // });
+
+// const aboutPartner = document.querySelector(".about__partners");
+
+// const message = [];
+// switch (message) {
+//   case `<p>
+//   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+// </p>
+// <h3>
+//   Mohanad Eaton
+// </h3>
+// <span>co-founder</span>`:
+//     break;
+//   case `<p>
+//   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+// </p>
+// <h3>
+//   Aarman Cox
+// </h3>
+// <span>co-founder</span>`:
+//     break;
+// }
+
+// partners section
+const partnerContent = document.getElementById("partner-content");
+const prevBtn = document.querySelector(".prev__btn");
+const nextBtn = document.querySelector(".next__btn");
+
+const messages = [
+  `<p>
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    <h3>
+      Mohanad Eaton
+    </h3>
+    <span>co-founder</span>`,
+  `<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, placeat eos? Enim nam saepe ullam, quam, exercitationem quidem et quis suscipit doloribus tenetur ea voluptate veritatis cumque, fugiat similique est!
+  Eaque provident eos dolore distinctio perferendis unde nisi nostrum ad vitae, cupiditate facilis repudiandae aperiam placeat officia eius? Aspernatur, suscipit laborum iste vitae nostrum eius ut enim id eligendi voluptatum!
+    </p>
+    <h3>
+      Aarman Cox
+    </h3>
+    <span>co-founder</span>`,
+];
+
+let currentPartnerIndex = 0;
+
+function updateContent() {
+  partnerContent.innerHTML = messages[currentPartnerIndex];
+}
+
+function toggleActiveButton() {
+  prevBtn.classList.remove("btn__active");
+  nextBtn.classList.remove("btn__active");
+
+  if (currentIndex === 0) {
+    prevBtn.classList.add("btn__active");
+  } else {
+    nextBtn.classList.add("btn__active");
+  }
+}
+
+prevBtn.addEventListener("click", () => {
+  currentPartnerIndex =
+    currentPartnerIndex === 0 ? messages.length - 1 : currentPartnerIndex - 1;
+  updateContent();
+});
+
+nextBtn.addEventListener("click", () => {
+  currentPartnerIndex =
+    currentPartnerIndex === messages.length - 1 ? 0 : currentPartnerIndex + 1;
+  nextBtn.classList.add("btn__active");
+  updateContent();
+});
+
+// Initialize with the first content
+updateContent();
