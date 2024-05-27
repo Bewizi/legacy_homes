474259c0ff5b36d49c2f46490407c734d8d4a986
Bewizi <toluwalase.bewaji@yahoo.com>
2024-05-27 20:47:58 +0100

    worked on the newsletter--section

--- script.js
+++ script.js
@@ -172,3 +172,25 @@ nextBtn.addEventListener("click", () => {
 
 // Initialize with the first content
 updateContent();
+
+// Reveal sections
+const allSections = document.querySelectorAll(".section");
+
+const revelSection = function (entries, observe) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observe.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revelSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
